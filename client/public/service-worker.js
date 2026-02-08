d1cdaeaedbc93483eb407c8e266e6d107f4c09d1
sanjay-1221342 <sanjayayyakkannu474@gmail.com>
2026-02-08 20:17:49 +0530

    Fix PWA cache issue on Vercel

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,15 +1,16 @@
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
